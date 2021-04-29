2ba8ebe3a2c79eef3296607ca5be05b83a3cfe3f
Amrit Kumar <kumaramrit38@gmail.com>
2021-04-29 10:35:25 +0530

    crud operation added

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,13 +1,13 @@
-package com.example.demo.controller;
+package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class HomeController {
-
-    @GetMapping("/")
-    public String hello(){
-        return "hello";
-    }
-}
+//@Controller
+//public class HomeController {
+//
+//    @GetMapping("/")
+//    public String hello(){
+//        return "hello";
+//    }
+//}
